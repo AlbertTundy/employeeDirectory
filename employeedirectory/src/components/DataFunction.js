479db68a3385c99c919535c329680eb8387562af
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-31 16:12:00 -0600

    worked on DataFunction.js added psudo coding

---
+++ employeedirectory/src/components/DataFunction.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Axios from "../axios"
+import MainTable from "./MainTable"
+import NavBar from "./NavBar"
+
+export default class DataFunction extends React.Component
